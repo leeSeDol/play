5eedc8cc807a01c1c1e9a854d3591e844dd23e83
我喜欢雅雅 <3075139617@qq.com>
2017-03-23 17:06:40 +0800

    Update script.js

--- HTML5 clock/script.js
+++ HTML5 clock/script.js
@@ -6,6 +6,7 @@ window.onload=function(){
 	var r = width/2;
 	console.log(r);
 	function drawBackground(){
+		ctx.save();		
 		ctx.translate(r,r);
 		ctx.beginPath();
 		ctx.lineWidth=10;
@@ -38,10 +39,67 @@ window.onload=function(){
 			}
 			
 			ctx.fill();
-
 		}
 	}
-	drawBackground();
+	function drawHour(hour,minute){
+		ctx.save();
+		ctx.beginPath();
+		var rad = 2*Math.PI/12*hour;
+		var mrad = 2*Math.PI/12/60*minute;
+		ctx.rotate(rad+mrad);
+		ctx.lineWidth=6;
+		ctx.lineCap='round';
+		ctx.moveTo(0,10);
+		ctx.lineTo(0,-r/2);
+		ctx.stroke();
+		ctx.restore();
+	}
+	function drawMinute(minute){
+		ctx.save();
+		ctx.beginPath();
+		var rad=2*Math.PI/60*minute;
+		ctx.rotate(rad);
+		ctx.lineWidth=3;
+		ctx.lineCap='round';
+		ctx.moveTo(0,10);
+		ctx.lineTo(0,-r+43);
+		ctx.stroke();
+		ctx.restore();
+	}
+	function drawSecond(second){
+		ctx.save();
+		ctx.beginPath();
+		var rad=2*Math.PI/60*second;
+		ctx.rotate(rad);
+		ctx.moveTo(-2,20);
+		ctx.lineTo(2,20);
+		ctx.lineTo(-1,-r+30);
+		ctx.lineTo(1,-r+20);
+		ctx.fillStyle="#391";
+		ctx.fill();
+		ctx.restore();
+	}
+	function drawDot(){
+		ctx.beginPath();
+		ctx.fillStyle="#fff";
+		ctx.arc(0,0,3,0,2*Math.PI,false);
+		ctx.fill();
+	}
+
+	function draw(){
+		ctx.clearRect(0,0,width,height);
+		var now = new Date();
+		var hour = now.getHours();
+		var minute = now.getMinutes();
+		var second = now.getSeconds();
+		drawBackground();
+		drawHour(hour,minute);
+		drawMinute(minute);
+		drawSecond(second);
+		drawDot();
+		ctx.restore();
+	}
+	setInterval(draw,100);
 
 
 };
