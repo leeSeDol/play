6d3addedff390e0f5f333605706cc04f2864d8ff
我喜欢雅雅 <3075139617@qq.com>
2017-04-03 11:20:05 +0800

    Update carousel.js

--- Carousal/carousel.js
+++ Carousal/carousel.js
@@ -17,9 +17,67 @@
 		};
 		$.extend(this.setting,this.getSetting());
 		this.setSettingValue();
+		this.setPosterPos();
 	};
 	Carousel.prototype={
-		
+		//设置剩余的帧的位置关系
+		setPosterPos:function(){
+			var self = this;
+			var sliceItems = this.posterItems.slice(1);
+			var sliceSize = Math.floor(sliceItems.size()/2);
+			var rightSlice=sliceItems.slice(0,sliceSize);
+			var leftSlice=sliceItems.slice(sliceSize);
+			var level=Math.floor(sliceItems.size()/2);
+			//设置右边帧的位置关系和宽度高度
+			var rw=this.setting.posterWidth;
+			var rh=this.setting.posterHeight;
+			var gap=((this.setting.width-this.setting.posterWidth)/2)/level;
+			var firstLeft=(this.setting.width-this.setting.posterWidth)/2;
+			var firstOffsetLeft=firstLeft+rw;
+			rightSlice.each(function(i){
+				level--;
+				rw=rw*self.setting.scale;
+				rh=rh*self.setting.scale;
+				var j=i;
+				$(this).css({
+					zIndex:level,
+					opacity:1/(++j),
+					width:rw,
+					height:rh,
+					left:firstOffsetLeft+(++i)*gap-rw,
+					top:self.setVerticalAlign(rh)
+				});
+			});
+			var lw=rightSlice.last().width();
+			var lh=rightSlice.last().height();
+			var oloop=Math.floor(this.posterItems.size()/2);
+			leftSlice.each(function(i){
+				$(this).css({
+					width:lw,
+					height:lh,
+					opacity:1/oloop,
+					zIndex:i,
+					left:(gap)*i++,
+					top:self.setVerticalAlign(lh)
+				});
+				lw=lw/self.setting.scale;
+				lh=lh/self.setting.scale;
+				oloop--;
+			});
+		},
+		//设置垂直排列对其
+		setVerticalAlign:function(height){
+			var verticalType=this.setting.verticalAlign;
+			var top=0;
+			if(verticalType=="top"){
+				top=0;
+			}else if(verticalType=="middle"){
+				top=(this.setting.height-height)/2;
+			}else if(verticalType=="bottom"){
+				top=(this.setting.height-height);
+			}
+			return top;
+		},
 		
 		//设置配置参数值去设置基本的宽度高度
 		setSettingValue:function(){
@@ -45,6 +103,12 @@
 				width:w,
 				height:this.setting.height,
 				zIndex:Math.ceil(this.posterItems.size())
+			});
+			this.posterFirstItem.css({
+				width:this.setting.posterWidth,
+				height:this.setting.posterHeight,
+				left:w,
+				zIndex:(this.posterItems.size())
 			});
 		},
 		getSetting:function(){
