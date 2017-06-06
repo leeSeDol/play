fe00f52b666f0addcb67e9016df2cd7cf5d5c027
蓝天任我翱翔 <3075139617@qq.com>
2017-06-06 21:32:22 +0800

    Update script.js

--- 5_lineChart/script.js
+++ 5_lineChart/script.js
@@ -25,8 +25,46 @@ window.onload=function(){
 	ctx.lineTo(padding+arrowW/2,padding+arrowH/2);
 	/*横方向箭头*/
 	ctx.moveTo(w-padding+arrowW/2,h-padding);
-	ctx.lineTo();
-	ctx.lineTo();
-	ctx.lineTo();
+	ctx.lineTo(w-padding-arrowW/2,h-padding-arrowH/2);
+	ctx.lineTo(w-padding,h-padding);
+	ctx.lineTo(w-padding-arrowW/2,h-padding+arrowH/2);
 	ctx.fill();
+
+	/*封装画小方块的函数*/
+	function drawPoint(x,y,r,color){
+		r=r||8;
+		color = color || '#000';
+		ctx.beginPath();
+		ctx.moveTo(x-r,y-r);
+		ctx.lineTo(x+r,y-r);
+		ctx.lineTo(x+r,y+r);
+		ctx.lineTo(x-r,y+r);
+		ctx.fillStyle=color;
+		ctx.fill();
+	}
+
+	//drawPoint(80,40);
+	var points=[ [20,80],[40,1200],[80,80],[200,300],[400,150] ];
+
+	var x0=padding,
+		y0=h-padding;
+
+	/*按从小到大排列*/
+	points.sort(function(a,b){
+		return a[0]-b[0];
+	});
+
+	var xLength = w-2*padding-arrowW ;
+	var yLength = h-2*padding-arrowH ;
+
+	console.log(xLength,yLength);
+	/*求出y坐标的最大值*/
+	yMax = Math.max.apply(null,points.map(function(ele){
+		return ele[1];
+	}));
+	/*求出x坐标的最大值*/
+	xMax = points[points.length-1][0];
+	
+	/*得到每一个小方块的位置*/
+	
 };
