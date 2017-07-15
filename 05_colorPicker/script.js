a1c90473e594ba0d31c458d9ec207debf895e044
李达康 <3075139617@qq.com>
2017-07-15 20:48:11 +0800

    Update script.js

--- 05_colorPicker/script.js
+++ 05_colorPicker/script.js
@@ -9,8 +9,8 @@ function ColorBar(ele , width , height , result , textObject){
 };
 ColorBar.prototype.drawPanel=function(){
 	var ele=$(this.ele);
-	var left = e.position().left;
-	var top = e.position().top;
+	var left = ele.position().left;
+	var top = ele.position().top;
 	var deltaC=0;
 	for(var x=0;x<this.width;x++){
 		deltaC =(x-0) * 255 * 6/(this.width);
@@ -18,7 +18,7 @@ ColorBar.prototype.drawPanel=function(){
 		ele.append(colorPx);
 
 		colorPx.click(function(){
-			var bgColor=$(this).css({"background-color"});
+			var bgColor=$(this).css("background-color");
 			$(self.result).css({"background-color":bgColor});
 			$(self.textObject).text(bgColor);
 		});
@@ -27,13 +27,63 @@ ColorBar.prototype.drawPanel=function(){
 			"position":"absolute",
 			"left":x+left,
 			"top":0+top,
+			"width":1,
 			"height":this.height,
 		});
+		if (deltaC>=0 && deltaC<=255) {
+			r = 255;
+			g = 0;
+			b = 0;
+			colorPx.css(
+				"background-color","rgb("+r+","+(g+deltaC)+","+b+")"
+			);
+		}
+
+		if (deltaC>255 && deltaC<=255*2) {
+			r = 255;
+			g = 255;
+			b = 0;
+			colorPx.css(
+				"background-color","rgb("+(r-deltaC+255)+","+g+","+b+")"
+			);
+		}
+		if (deltaC>255*2 && deltaC<=255*3) {
+			r = 0;
+			g = 255;
+			b = 0;
+			colorPx.css(
+				"background-color","rgb("+r+","+g+","+(b+deltaC-255*2)+")"
+			);
+		}
+		if (deltaC>255*3 && deltaC<=255*4) {
+			r = 0;
+			g = 255;
+			b = 255;
+			colorPx.css(
+				"background-color","rgb("+r+","+(g-deltaC+255*3)+","+b+")"
+			);
+		}
+		if (deltaC>255*4 && deltaC<=255*5) {
+			r = 0;
+			g = 0;
+			b = 255;
+			colorPx.css(
+				"background-color","rgb("+(r+deltaC-255*4)+","+g+","+b+")"
+			);
+		}
+		if (deltaC>255*5 && deltaC<=255*6) {
+			r = 255;
+			g = 0;
+			b = 255;
+			colorPx.css(
+				"background-color","rgb("+r+","+g+","+(b-deltaC+255*5)+")"
+			);
+		}
 	}
 
 };
 
-(function(){
-	var colorBar = new colorBar( '#color_panel' , 255*3 , 165 ,'#color_result','#colorText');
+$(function(){
+	var colorBar = new ColorBar( '#color_panel' , 255*3 , 165 ,'#color_result','#colorText');
 	colorBar.drawPanel();
 });
