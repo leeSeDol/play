c3ea769119e395041692f12eeefacc52e77df4dc
李达康 <3075139617@qq.com>
2017-07-15 16:38:58 +0800

    Create script.js

---
+++ 05_colorPicker/script.js
@@ -0,0 +1,34 @@
+var self;
+function ColorBar(ele , width , height , result , textObject){
+	this.ele=ele;
+	this.width=width;
+	this.height=height;
+	this.result=result;
+	this.textObject=textObject;
+	self=this;
+};
+ColorBar.prototype.drawPanel=function(){
+	var ele=$(this.ele);
+	var left = e.position().left;
+	var top = e.position().top;
+	var deltaC=0;
+	for(var x=0;x<this.width;x++){
+		deltaC =(x-0) * 255 * 6/(this.width);
+		var colorPx = $("<div></div>");
+		ele.append(colorPx);
+
+		colorPx.click(function(){
+			var bgColor=$(this).css({"background-color"});
+			$(self.result).css({"background-color":bgColor});
+			$(self.textObject).css({"background-color":bgColor});
+		});
+
+		colorPx.css({
+			"position":"absolute",
+			"left":x+left,
+			"top":0+top,
+			"height":this.height,
+		});
+	}
+
+};
