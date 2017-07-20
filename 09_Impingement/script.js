1d195e6253e1f43515e7056392771d123cc8e17c
李达康 <3075139617@qq.com>
2017-07-20 17:51:04 +0800

    Update script.js

--- 09_Impingement/script.js
+++ 09_Impingement/script.js
@@ -4,8 +4,36 @@ function init(){
 	var ball = document.getElementById("ball");
 	var posX = 0;
 	var posY = 0;
-
-
-
-
+	var moveX = true;
+	var moveY = true;
+	time = null;
+	function move(){
+		if(moveX){
+			posX++;
+			ball.style.left = posX+'px';
+			if(posX == main.offsetWidth - ball.clientWidth){
+				moveX = false;
+			}
+		}else{
+			posX--;
+			ball.style.left = posX+'px';
+			if(posX == 0){
+				moveX = true;
+			}
+		}
+		if(moveY){
+			posY++;
+			ball.style.top = posY+'px';
+			if(posY == main.offsetHeight - ball.offsetHeight){
+				moveY = false;
+			}
+		}else{
+			posY--;
+			ball.style.top = posY+'px';
+			if(posY == 0){
+				moveY = true;
+			}
+		}
+	}
+	time = setInterval(move,10);
 }
