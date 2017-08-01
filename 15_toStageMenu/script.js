5dedb0b1ac9a3e741dfeab435ea9da27f92062f0
李达康 <3075139617@qq.com>
2017-08-01 08:52:53 +0800

    Update script.js

--- 15_toStageMenu/script.js
+++ 15_toStageMenu/script.js
@@ -2,7 +2,14 @@
 	$(".Menu ul li").hover(function(){
 		//鼠标划上去
 		$(this).addClass("hover");
-
+		var _top = $(this).position().top;
+		var _height = $(this).find(".nav_more").height()/2;
+		var clientHeight = document.documentElement.clientHeight || document.body.clientHeight; 
+		console.log(_top);
+		console.log(_height);
+		if( _top+_height*2 > clientHeight-80){
+			$(this).find(".nav_more").css("top","-"+_height+"px");
+		}
 	},function(){
 		//鼠标移开
 		$(this).removeClass("hover");
